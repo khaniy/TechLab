f05bf118789922fb73396c67dd3ceebac724a3a6
khaniy <kangh1596@gmail.com>
2024-03-05 17:32:11 +0900

    [INIT] SparkLab 생성

---
+++ SparkLab/src/test/java/han/sparklab/SparkLabApplicationTests.java
@@ -0,0 +1,13 @@
+package han.sparklab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
